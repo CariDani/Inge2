9ef2ce02fb3506ce662f05299b5f059dd2ce68f9
cavila31 <cavila31@hotmail.com>
2016-04-17 11:07:15 -0600

    proyecto basico

---
+++ DC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DC.Startup))]
+namespace DC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
